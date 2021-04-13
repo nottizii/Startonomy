6a1aea354c776473dc51b326643459b0fc5240bc
xAtsuUC <bruzianoytvideos@gmail.com>
2021-04-13 19:05:50 -0300

    Fixed Syntaz

--- index.js
+++ index.js
@@ -15,13 +15,14 @@ class economyManager {
     async getData(userID, guildID) {
         this.guildID = guildID ?? null
         this.userID = userID ?? null
-        this.data = await this.queryDatabase({guildID: this.guildID, userID: this.userID})
+        this.data = await this._queryDatabase({guildID: this.guildID, userID: this.userID})
         return this.data;
     }
     
     _queryDatabase(payload) {
         return new Promise((resolve, reject) => {
             this.connection.getConnection((sqlerr, con) => {
+                if(sqlerr) throw sqlerr
                 con.query(`SELECT * FROM data_${payload.guildID} WHERE id = '${payload.userID}'`, (err, rows) => {
                     if(err) reject(err);
                     con.release()
@@ -81,7 +82,7 @@ class economyManager {
     registerguild(guildID) {
         this.guildID = guildID ?? null
         this.connection.getConnection((err, con) => {
-            con.query(`CREATE TABLE IF NOT EXISTS 'data_${this.guildID}' ( 'id' varchar(18) CHARACTER SET utf8 DEFAULT 'No id',  'bank' varchar(9) CHARACTER SET utf8 DEFAULT '0',  'wallet' varchar(9) CHARACTER SET utf8 DEFAULT '0',  'unlimited' varchar(5) CHARACTER SET utf8 DEFAULT 'false') ENGINE=InnoDB DEFAULT CHARSET=latin1`)
+            con.query(`CREATE TABLE IF NOT EXISTS data_${this.guildID} (id varchar(18) CHARACTER SET utf8 DEFAULT 'No id', bank varchar(9) CHARACTER SET utf8 DEFAULT '0',  wallet varchar(9) CHARACTER SET utf8 DEFAULT '0', unlimited varchar(5) CHARACTER SET utf8 DEFAULT 'false') ENGINE=InnoDB DEFAULT CHARSET=latin1`, (err) => { if(err) throw err })
             con.release();
         })
     }
@@ -89,8 +90,14 @@ class economyManager {
         this.userID = userID ?? null
         this.guildID = guildID ?? null
         this.connection.getConnection((err, con) => {
-            con.query(`INSERT INTO 'data_${this.guildID}' ('id') VALUES ('${this.userID}')`)
+            if(err) throw err
+            con.query(`CREATE TABLE IF NOT EXISTS data_${this.guildID} (id varchar(18) CHARACTER SET utf8 DEFAULT 'No id', bank varchar(9) CHARACTER SET utf8 DEFAULT '0',  wallet varchar(9) CHARACTER SET utf8 DEFAULT '0', unlimited varchar(5) CHARACTER SET utf8 DEFAULT 'false') ENGINE=InnoDB DEFAULT CHARSET=latin1`, (err) => { if(err) throw err })
             con.release();
+            this.connection.getConnection((err, con) => {
+                if(err) throw err
+                con.query(`INSERT INTO data_${this.guildID} SET id='${this.userID}'`, (err) => { if(err) throw err })
+                con.release();
+            })
         })
     }
 }
