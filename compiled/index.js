c89b5190e57b1d744d1a37b9f926ae705fbab7a6
Tiziano Fioretti <tiziafioretti@gmail.com>
2024-03-24 11:18:09 -0300

    I honestly don't understand what old me was doing so might aswell start over

--- compiled/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const economyManager_1 = require("./economyManager");
-module.exports = { economyManager: economyManager_1.economyManager };
